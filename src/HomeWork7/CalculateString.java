88caf21557346837af067c078ca9317261b0ca54
Yulia_Tenchurina <teenager111@yandex.ru>
2021-08-19 17:26:26 +0300

    Создали метод вычисления результата работы выражения

--- src/HomeWork7/CalculateString.java
+++ src/HomeWork7/CalculateString.java
@@ -21,10 +21,14 @@ public class CalculateString {
                ArrayList <String> arrayStrok = new ArrayList<>();
                //передаем  текст для создания массива
                arrayStrok = FindParts(data,arrayStrok);
-               double rezultat = computation(arrayStrok);
-               System.out.println(String.format("Значением выражения %s является %f.0",data,rezultat));
+               if (arrayStrok.size()!=3){
+                   System.out.println("Вы ввели данные в неверном формате");
+               }
+               else {
+                   double rezultat = computation(arrayStrok);
+                   System.out.println(String.format("Значением выражения %s является %f.0",data,rezultat));
+               }
            }
-
         }catch (Exception e){
             System.err.println("Получили недопустимое выражение: "+ e);
         }
@@ -33,7 +37,6 @@ public class CalculateString {
     }
 
     public static ArrayList<String> FindParts(String text, ArrayList<String> arrayStrok){
-
         String regexProbel="[^\\s]+";
         Pattern datePatternProbel =Pattern.compile(regexProbel);
         Matcher findDateProbel = datePatternProbel.matcher(text);
@@ -45,6 +48,47 @@ public class CalculateString {
     }
 
     public static double computation(ArrayList <String> arrayStrok){
+        double ch1=0;
+        double ch2=0;
+        String operation = arrayStrok.get(1);
+        double rezult;
+        try {
+            ch1 = Double.parseDouble(arrayStrok.get(0));
+        }catch (Exception e){
+            System.err.println(String.format
+                    ("Первые символы( %s) -  не являются числом. Ошибка: %s",arrayStrok.get(0),e );
+        }
+
+        try {
+            ch2 = Double.parseDouble(arrayStrok.get(0));
+        }catch (Exception e){
+            System.err.println(String.format
+                    ("Последние символы( %s) -  не являются числом. Ошибка: %s",arrayStrok.get(2),e );
+        }
+
+        if (operation.equals("плюс")) {
+            return (ch1+ch2);
+        }
+        else  if(operation.equals("минус")){
+            return (ch1-ch2);
+        }
+        else  if(operation.equals("умножить")){
+            return (ch1*ch2);
+        }
+        else  if(operation.equals("делить")){
+            try {
+                rezult = ch1/ch2;
+                return rezult;
+            }catch (Exception e){
+                System.err.println("Делить на 0 нельзя"+ e );
+            }
+           // return (0);
+        }
+        else {
+            System.out.println("Вы ввели недопустимую арифметическую операцию");
+        }
+
+
 
     }
 }
